358ae1c1d0e98914928b5f45fb46e79c68eca608
summerwya <cl30bell2003@outlook.com>
2026-01-10 18:29:09 +0800

    Added wrist to remove, IK in development

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ bpy.ops.object.mode_set(mode='EDIT')
 
 def delete_useless_bones(armature):
     # TODO - Optimize regex
-    # TODO - Investigate if _Handles should be removed
-    pattern = re.compile(r'Sp_(Hi|So)_.*0_[RL]_01|Sp_He_Ear0_[RL]_02|Sp_Ch_Collar0_[RL]_01|Sp_He_Hair2_[RL]_04|Sp_Hi_CSkirt[01]_([RL]|[FB][RL]|[FB][RL][RL])_0[12]|_Handle$')
+    pattern = re.compile(r'Sp_(Hi|So)_.*0_[RL]_01|Sp_He_Ear0_[RL]_02|Sp_Ch_Collar0_[RL]_01|Sp_He_Hair2_[RL]_04|Sp_Hi_CSkirt[01]_([RL]|[FB][RL]|[FB][RL][RL])_0[12]|_Handle$|^Wrist_[RL]_')
     dont_delete = ['Sp_Hi_CSkirt0_FLL_01', 'Sp_Hi_CSkirt0_FRR_01']
     
     edit_bones = armature.data.edit_bones
@@ -26,10 +25,10 @@ def delete_useless_bones(armature):
     print(f'Deleted {len(bones_to_delete)} bones')
 
 def correct_bone_names(armature):
-    pass # TODO - Use a csv file
+    pass # TODO - Implement this function
 
 def correct_finger_bone_rotation(armature):
-    pass # TODO - Just rotate the fingers a bit instead of making them pointed directly up
+    pass # TODO - Implement this function
 
 def hide_face_bones(armature):
     pattern = re.compile('Eye_|Eyebrow_|Mouth_|Tooth_')
@@ -44,11 +43,15 @@ def hide_face_bones(armature):
     for bone in armature.data.bones:
         if pattern.search(bone.name):
             armature.data.bones[bone.name].hide = True
+
+def fixIK(armature):
+    pass # Add leg IK, foot IK
             
 
 # Start the cleaning process
 delete_useless_bones(armature)
 hide_face_bones(armature)
+fixIK(armature)
 correct_bone_names(armature)
 correct_finger_bone_rotation(armature)
 
